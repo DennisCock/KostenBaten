020c7f6b43ec77701af27b7d0a2105affb835323
Dennis Cock <dennis.cock.t3749@student.hogent.be>
2017-03-02 09:59:02 +0100

    Aanmelden/Registreren + Begin mockup

--- src/KostenBaten/wwwroot/js/site.js
+++ src/KostenBaten/wwwroot/js/site.js
@@ -1,1 +1,8 @@
 ﻿// Write your Javascript code.
+$('.message a').click(function () {
+    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
+
+function registreer() {
+    alert("Een bevestigingsmail werd verstuurd naar uw emailadres.")
+}
